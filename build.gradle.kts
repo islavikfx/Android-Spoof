ed54179a5b13435a7dac5831f34668c69a40c5a3
islavikfx <rikka879387@gmail.com>
2026-05-18 20:00:13 +0300

    Update SDK for compile (project)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,14 @@
 plugins {
-    id("com.android.application") version "9.2.0" apply false
+    id("com.android.application") version "9.2.1" apply false
     id("org.jetbrains.kotlin.android") version "2.3.21" apply false
 }
+
+
+ext {
+    set("compileSdk", 37)
+    set("minSdk", 29)
+}
+
 
 tasks.register("clean", Delete::class) {
     delete(rootProject.layout.buildDirectory)
