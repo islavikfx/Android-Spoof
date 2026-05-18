d7a071e8283d5ef169033f646c8b18509078ccf5
islavikfx <rikka879387@gmail.com>
2026-05-18 20:12:47 +0300

    Updated InstrumentedTest.kt

--- app/src/androidTest/java/com/islavikfx/spoof/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/islavikfx/spoof/ExampleInstrumentedTest.kt
@@ -1,4 +1,8 @@
 package com.islavikfx.spoof
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.ext.junit.rules.ActivityScenarioRule
@@ -14,10 +18,15 @@ class InstrumentedTest {
 
     @get:Rule
     val activityRule = ActivityScenarioRule(AppActivity::class.java)
+
+    @Test
+    fun testActivityIsLaunching() {
+        onView(withId(android.R.id.content)).check(matches(isDisplayed()))
+    }
+
     @Test
     fun useAppContext() {
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.islavikfx.spoof", appContext.packageName)
-
     }
 }
