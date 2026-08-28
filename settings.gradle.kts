f1d8558f83ff9a84bdc37d54a949808fa5fda179
islavikfx <williamssmith251@outlook.com>
2026-08-29 01:45:33 +0300

    Added project gradle settings

---
+++ settings.gradle.kts
@@ -0,0 +1,31 @@
+@file:Suppress("UnstableApiUsage")
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+        maven {
+            url = uri("https://jitpack.io")
+            content {
+                includeGroup("com.github.islavikfx")
+                includeGroup("com.github.topjohnwu")
+                includeGroup("com.github.topjohnwu.libsu")
+                includeModule("com.github.topjohnwu.libsu", "core")
+                includeModule("com.github.topjohnwu.libsu", "service")
+                includeModule("com.github.topjohnwu.libsu", "nio")
+            }
+        }
+    }
+}
+
+
+rootProject.name = "Android Spoof"
+include(":app")
